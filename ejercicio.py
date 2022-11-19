87f098a5f9ee176ec00ac4a1a070af238bd611b5
mariapa12 <mariapa12@hotmail.es>
2022-11-19 12:12:50 -0500

    prueba

--- ejercicio.py
+++ ejercicio.py
@@ -3,49 +3,45 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 
-#negativo
+#########################################
+#Negativo
 #given
-driver = webdriver.Chrome(executable_path=r"chromedriver.exe")
+driver = webdriver.Chrome(executable_path='chromedriver.exe')
 driver.get("http://www.python.org")
 #assert "Python" in driver.title
 #when
-elem=driver.find_element(By.NAME, "q")
+elem = driver.find_element(By.NAME, "q")
 elem.clear()
-elem.send_keys ("variable")
+elem.send_keys("varible")
 
-boton= driver.find_element (By.ID,"submit")
+boton = driver.find_element(By.ID, "submit")
 boton.click()
-time.sleep (5)
+time.sleep(5)
 
-#then
-#resultado=driver.find_element_by_xpath ("//*[@id='content']/div/section/form/ul/p")
-#no results found.
-#resultado
-driver.close()
-#positivo 
-driver = webdriver.Chrome(executable_path=r"chromedriver.exe")
-driver.get("http://www.python.org")
-#assert "Python" in driver.title
-#when
-elem=driver.find_element(By.NAME, "q")
-elem.clear()
-elem.send_keys ("variable")
-
-boton= driver.find_element (By.ID,"submit")
-boton.click()
-time.sleep (5)
-driver.close()
-
-**negativo**
 #then
 resultado = driver.find_element(By.XPATH, "//*[@id='content']/div/section/form/ul/p")
 assert resultado.text == 'No results found.'
 time.sleep(3)
+
 driver.close()
- 
-**positivo**
+
+#########################################
+#Positivo
+#given
+driver = webdriver.Chrome(executable_path='chromedriver.exe')
+driver.get("http://www.python.org")
+#when
+elem = driver.find_element(By.NAME, "q")
+elem.clear()
+elem.send_keys("variable")
+
+boton = driver.find_element(By.ID, "submit")
+boton.click()
+time.sleep(5)
+
 #then
 resultado = driver.find_element(By.XPATH, "//*[@id='content']/div/section/form/ul/li[1]/h3/a")
 assert resultado.text == 'PEP 526 -- Syntax for Variable Annotations'
 time.sleep(3)
+
 driver.close()
